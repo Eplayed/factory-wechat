02ce5e40d87763d8e36fd248ab3a584cb3123c1b
zhangyajun <454140094@qq.com>
2020-12-03 13:17:14 +0800

    update project packages

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,13 @@
+/*
+ * @Author: zyj
+ * @Date: 2020-09-03 15:09:43
+ * @LastEditors: zyj
+ * @LastEditTime: 2020-12-03 13:12:39
+ * @Description: file content
+ * @FilePath: /factorynike-mini/src/app.ts
+ */
 import { Component } from 'react'
+import 'taro-ui/dist/style/index.scss'
 import './app.scss'
 
 class App extends Component {
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,7 +4,6 @@ import { View, Text, Swiper, SwiperItem } from "@tarojs/components";
 import { AtButton } from "taro-ui";
 import { setStore, getStore } from "@/utils/utils";
 
-import "taro-ui/dist/style/components/button.scss"; // 按需引入
 import "./index.scss";
 
 export default class Index extends Component {
