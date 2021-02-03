b584c854397b575f5403b1fd8fea74e71aff6ff1
zhangyajun <454140094@qq.com>
2021-02-03 15:54:15 +0800

    hooks update

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,45 +1,41 @@
 /*
  * @Author: zyj
- * @Date: 2020-09-03 15:09:43
+ * @Date: 2020-12-14 14:28:58
  * @LastEditors: zyj
- * @LastEditTime: 2021-02-03 15:44:06
+ * @LastEditTime: 2021-02-03 15:53:53
  * @Description: file content
  * @FilePath: /factorynike-mini/src/pages/mine/index.tsx
  */
-import React, { Component } from "react";
-import { View, Text, Swiper, SwiperItem, Image } from "@tarojs/components";
-import { AtList,AtListItem } from "taro-ui";
-import Taro from '@tarojs/taro'
-
+import Taro, { FC } from "@tarojs/taro";
+import { View, ScrollView, Button } from "@tarojs/components";
+import { AtList, AtListItem } from "taro-ui";
+import React, { useState, useEffect } from "react";
 import "./index.scss";
+// 函数式组件
+const Page = () => {
+  const [userId] = useState<number>(Taro.getStorageSync("userId"));
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  useEffect(() => {
+    getFollowedList();
+  }, []);
 
-type StateType = {
-  [propName: string]: any;
+  function getFollowedList() {
+    if (!hasMore) return;
+    console.log(12);
+  }
+
+  return (
+    <View className="index">
+      <ScrollView>
+        <View className="index">
+          <AtList>
+            <AtListItem title="姓名" extraText="张丽" />
+            <AtListItem title="电话" extraText="1398089090809" />{" "}
+          </AtList>
+        </View>
+      </ScrollView>
+    </View>
+  );
 };
 
-export default class Index extends Component {
-  state: StateType = {
-   
-  };
-  componentWillMount() {}
-
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  render() {
-    return (
-      <View className="index">
-        <AtList>
-
-  <AtListItem title='姓名' extraText='张丽' />
-  <AtListItem title='电话' extraText='1398089090809' />
-</AtList>
-      </View>
-    );
-  }
-}
+export default Page;
